de2038cd60fa30acfd9009f7774b30b3c6b90093
achsanalfitra <gethwulf@gmail.com>
2024-05-08 09:51:04 +0700

    Prototyped data structure for multilevel task

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 """This is the main file"""
 
-def object_multilevel_task(given_level, given_task):
-    """This function is used to define the multilevel_task object"""
-    tasks = []
-    current_level = []
-    for level in given_level:
-        tasks.append(given_task)
+current_dictionary = {}
+
+def multilevel_dictionary(current_dictionary, given_level, given_task, previous_task=None):
+    """This function is a prototype of multilevel dictionary"""
+    if given_level == 1:
+        current_dictionary.setdefault(given_task, given_task)
+    else:
+        temp_dictionary = {}
+        temp_dictionary.setdefault(given_task, given_task)
+        current_dictionary[previous_task] = temp_dictionary
+    return current_dictionary
+
+current_dictionary = multilevel_dictionary(current_dictionary, 1, "eating")
+current_dictionary = multilevel_dictionary(current_dictionary, 1, "going")
+current_dictionary = multilevel_dictionary(
+    current_dictionary, 2, "grocery shopping", previous_task="going")
+current_dictionary = multilevel_dictionary(current_dictionary, 1, "laughing")
+current_dictionary = multilevel_dictionary(current_dictionary, 1, "talking")
+print(current_dictionary)
 
 # End-of-file (EOF)
